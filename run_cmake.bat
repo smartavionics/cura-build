f1c3c78307c1def55166e5af25c92ace99b4a889
Mark Burton <markb@smartavionics.com>
2018-10-05 18:29:56 +0100

    New file.

---
+++ run_cmake.bat
@@ -0,0 +1,10 @@
+
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
+
+set INSTALL_DIR=%HOMEDRIVE%%HOMEPATH%\cura\inst
+
+set PATH=%INSTALL_DIR%\bin;%PATH%
+set PATH=%INSTALL_DIR%\scripts;%PATH%
+set PATH=%INSTALL_DIR%;%PATH%
+
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=%INSTALL_DIR% -DCMAKE_INSTALL_PREFIX=%INSTALL_DIR% -DBUILD_OS_WIN64=ON -G "NMake Makefiles" -DCURA_VERSION=mb-master ..
