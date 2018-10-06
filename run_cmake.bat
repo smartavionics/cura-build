92a039f5d9c2aa794f89c578a78d9de095745e69
Mark Burton <markb@smartavionics.com>
2018-10-06 15:11:35 +0100

    Need to set PYTHONPATH.

--- run_cmake.bat
+++ run_cmake.bat
@@ -7,4 +7,7 @@ set PATH=%INSTALL_DIR%\bin;%PATH%
 set PATH=%INSTALL_DIR%\scripts;%PATH%
 set PATH=%INSTALL_DIR%;%PATH%
 
+set PYTHONPATH=%cd%\inst\lib\python3.5\site-packages
+set PATH=%cd%\inst\bin;%PATH%
+
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=%INSTALL_DIR% -DCMAKE_INSTALL_PREFIX=%INSTALL_DIR% -DBUILD_OS_WIN64=ON -G "NMake Makefiles" -DCURA_VERSION=mb-master ..
