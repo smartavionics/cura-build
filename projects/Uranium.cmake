c5d4e3712a6f571c57f103058c819b2bc0a1c20c
Mark Burton <markb@smartavionics.com>
2019-01-19 11:04:35 +0000

    Use mb-master-4.0

--- projects/Uranium.cmake
+++ projects/Uranium.cmake
@@ -8,8 +8,8 @@ find_package(PyQt 5.6 REQUIRED)
 find_package(SciPy 0.17 REQUIRED)
 
 ExternalProject_Add(Uranium
-    GIT_REPOSITORY https://github.com/ultimaker/Uranium
-    GIT_TAG origin/${URANIUM_BRANCH_OR_TAG}
+    GIT_REPOSITORY https://github.com/smartavionics/Uranium
+    GIT_TAG mb-master-4.0
     CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${EXTERNALPROJECT_INSTALL_PREFIX} -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}
 )
 
