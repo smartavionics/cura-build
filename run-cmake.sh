57a2c53c351830e3b035d422636322aeffda15de
Mark Burton <markb@smartavionics.com>
2019-08-14 07:46:38 +0100

    Specify the Cura, CuraEngine and Uranium branches to build.

--- run-cmake.sh
+++ run-cmake.sh
@@ -4,4 +4,7 @@
 
 PATH=/opt/cura/bin:$PATH \
 PYTHONPATH=/opt/cura/lib/python3/dist-packages \
+CURA_BRANCH_OR_TAG=mb-master \
+CURAENGINE_BRANCH_OR_TAG=mb-master \
+URANIUM_BRANCH_OR_TAG=mb-master \
 cmake -DCMAKE_INSTALL_PREFIX:PATH=/opt/cura -DCMAKE_PREFIX_PATH=/opt/cura ..
