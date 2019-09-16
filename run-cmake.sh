db5fe5a2d661cfdf6b020d8d6094f8422f57b677
Mark Burton <markb@smartavionics.com>
2019-09-16 22:55:31 +0100

    Combine unix and osx versions.

--- run-cmake.sh
+++ run-cmake.sh
@@ -1,8 +1,18 @@
 #! /bin/sh
 
-PATH=~/cura/local/bin:$PATH \
-PYTHONPATH=~/cura/local/lib/python3/dist-packages \
+if [[ $(uname) == "Darwin" ]]; then
+  export CURA_TARGET_OSX_VERSION=10.13
+  . ../env_osx.sh
+  unset CC
+  unset CXX
+  export CURA_HOME=~/cura/local
+else
+  export CURA_HOME=/opt/cura
+fi
+
+PATH=$CURA_HOME/bin:$PATH \
+PYTHONPATH=$CURA_HOME/lib/python3/dist-packages \
 CURA_BRANCH_OR_TAG=mb-master \
 CURAENGINE_BRANCH_OR_TAG=mb-master \
 URANIUM_BRANCH_OR_TAG=mb-master \
-cmake -DCMAKE_INSTALL_PREFIX:PATH=~/cura/local -DCMAKE_PREFIX_PATH=~/cura/local -DCURA_VERSION=mb-master ..
+cmake -DCMAKE_INSTALL_PREFIX:PATH=$CURA_HOME -DCMAKE_PREFIX_PATH=$CURA_HOME -DCURA_VERSION=mb-master ..
