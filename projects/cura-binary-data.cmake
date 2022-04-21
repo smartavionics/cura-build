24d238e6c7199c71b4894c35dc8d586eb3fb3bdb
Mark Burton <burtoogle@gmail.com>
2022-04-21 08:39:48 +0100

    Fix project files.

--- projects/cura-binary-data.cmake
+++ projects/cura-binary-data.cmake
@@ -1,7 +1,7 @@
 ExternalProject_Add(cura-binary-data
     GIT_REPOSITORY https://github.com/ultimaker/cura-binary-data
-    GIT_TAG origin/${CURABINARYDATA_BRANCH_OR_TAG}
-    GIT_SHALLOW 1
+    GIT_TAG ${CURABINARYDATA_BRANCH_OR_TAG}
+    GIT_SHALLOW 0
     STEP_TARGETS update
     CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${EXTERNALPROJECT_INSTALL_PREFIX}
 )
